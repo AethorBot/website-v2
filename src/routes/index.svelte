6c2c53ee5bf08aba7fe53b6bfd0c3d43d680951b
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2021-12-31 12:42:29 +0100

    new: vercel things

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,10 @@
+<script>
+	export const ssr = true;
+	export const prerender = true;
+	export const hydrate = false;
+	export const router = false;
+</script>
+
 <article
 	class="p-4 prose lg:prose-xl prose-teal dark:prose-invert prose-headings:text-green-500 dark:prose-headings:text-green-400 prose-p:text-emerald-900 dark:prose-p:text-lime-200 prose-a:text-red-900 dark:prose-a:text-red-500"
 >
