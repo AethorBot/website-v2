c1812b7a8450dd4a8a4cb25f7cc77e608775990a
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2021-12-31 13:24:35 +0100

    404

---
+++ src/routes/404.svelte
@@ -0,0 +1,14 @@
+<script>
+	export const ssr = true;
+	export const prerender = true;
+	export const hydrate = false;
+	export const router = false;
+</script>
+
+<article
+	class="p-4 prose lg:prose-xl prose-teal dark:prose-invert prose-headings:text-green-500 dark:prose-headings:text-green-400 prose-p:text-emerald-900 dark:prose-p:text-lime-200 prose-a:text-red-900 dark:prose-a:text-red-500"
+>
+	<h1>404 not found</h1>
+	<p>Here take a frog instead</p>
+	<img src="/frog.jpg" alt="frog" />
+</article>
