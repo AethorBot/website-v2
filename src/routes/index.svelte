e9768d2d25407a9255c14dbaafde80722eb95dd5
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-01-02 19:17:03 +0100

    add feature list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,17 +5,69 @@
 	export const prerender = true;
 	export const hydrate = false;
 	export const router = false;
+
+	const features = [
+		{
+			name: 'suggestions channel',
+			description: 'Every message inside a suggestions channel becomes a suggestion.',
+			image: 'suggestions.png'
+		},
+		{
+			name: 'accepted channel',
+			description: 'You can set a channel for aethor where all accepted suggestions go into',
+			image: 'accepted.png'
+		},
+		{
+			name: 'denied channel',
+			description: 'You can set a channel for aethor where all denied suggestions go into',
+			image: 'denied.png'
+		},
+		{
+			name: 'Lots of suggestion commands',
+			description: 'Theres tons of commands related to suggestions',
+			image: 'suggestions-help.png'
+		}
+	];
 </script>
 
 <Paragraph>
 	<h2>Welcome to the aethor website</h2>
-	<p>Theres not much here besides the terms and privacy</p>
-	<p>Maybe there will be more at some point :D</p>
-	<p>
+	<p class="italic">The best suggestions bot</p>
+</Paragraph>
+
+<div class="w-full">
+	{#each features as feature, index}
+		<div
+			class={'md:flex lg:flex !prose-headings:text-black rounded-3xl w-full' +
+				(index % 2 ? 'ml-auto mr-0' : 'mr-auto ml-0')}
+		>
+			{#if index % 2}
+				<div class="m-auto">
+					<h3 class="text-7xl text-green-500">{feature.name}</h3>
+					<p class="text-2xl dark:text-green-600 text-blue-800">{feature.description}</p>
+				</div>
+				<div class="m-auto">
+					<img class="h-96 object-cover" alt={feature.name} src={`/features/${feature.image}`} />
+				</div>
+			{:else}
+				<div class="m-auto">
+					<img class="h-96 object-cover" alt={feature.name} src={`/features/${feature.image}`} />
+				</div>
+				<div class="m-auto">
+					<h3 class="text-7xl text-green-500">{feature.name}</h3>
+					<p class="text-2xl dark:text-green-600 text-blue-800">{feature.description}</p>
+				</div>
+			{/if}
+		</div>
+	{/each}
+</div>
+
+<Paragraph>
+	<!-- <p>
 		<a
 			href="https://discord.com/oauth2/authorize?client_id=870383692403593226&permissions=117824&scope=bot%20applications.commands"
 			>Invite aethor</a
 		> now
 	</p>
-	<img src="/frog.jpg" alt="frog" />
+	<img src="/frog.jpg" alt="frog" /> -->
 </Paragraph>
