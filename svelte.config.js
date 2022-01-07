8722cc2fdbe81ba925fda9d8c42524eba21cdb29
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-01-07 09:34:24 +0100

    stuff

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
-
+import { VitePWA } from 'vite-plugin-pwa';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
@@ -15,7 +15,28 @@ const config = {
 		adapter: adapter({ split: false }),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#main'
+		target: '#main',
+		vite: {
+			plugins: [
+				VitePWA({
+					includeAssets: ['favicon.svg', 'favicon.png', 'favicon.jpg', 'frog.jpg', 'robots.txt'],
+					manifest: {
+						name: 'Aethoe website',
+						short_name: 'Aethor',
+						description: 'Aethor is the best suggestions bot',
+						theme_color: '#0EC5FF',
+						background_color: '#0EC5FF',
+						icons: [
+							{
+								src: 'favicon.png',
+								sizes: '512x512',
+								type: 'image/png'
+							}
+						]
+					}
+				})
+			]
+		}
 	}
 };
 
