22f01d11b2182d92d7e46796c40bad429a3d2b3b
stef-the <stefthedoggo4@gmail.com>
2022-05-08 14:32:22 -0700

    PARALLAX

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,8 @@
 
 	import Addbutton from '../components/addbutton.svelte';
 
+	import { Parallax, ParallaxLayer } from 'svelte-parallax';
+
 	export const ssr = true;
 	export const prerender = true;
 	export const hydrate = false;
@@ -30,39 +32,45 @@
 			image: 'suggestions-help.png'
 		}
 	];
+	console.log(features.length+1)
 </script>
 
-
-<div class="py-72 bg-gradient-to-b dark:from-zinc-900 dark:to-slate-900 text-center">
-	<h1 class="text-4xl text-zinc-100 py-2">
-		Welcome to the Aethor website
-	</h1>
-	<p class="text-xl text-zinc-100 py-2"><b>
-		The best suggestions bot
-	</b></p>
-</div>
-
-<div class="w-full">
-	{#each features as feature, index}
-		<div
-			class={`md:flex w-full p-2 bg-zinc-800 my-10 ${
-				index % 2 ? 'md:ml-auto md:mr-0 text-right' : 'md:mr-auto md:ml-0 md:flex-row-reverse'
-			}`}
-		>
-			<div class="m-auto p-4">
-				<h3 class="md:text-5xl text-2xl dark:text-zinc-200 text-sky-700 py-3"><b>{feature.name}</b></h3>
-				<p class="md:text-2xl text-lg dark:text-zinc-400 text-blue-800">{feature.description}</p>
-			</div>
-			<div class="m-auto p-1">
-				<img
-					class="md:h-96 md:object-cover w-auto h-auto rounded-xl"
-					alt={feature.name}
-					src={`/features/${feature.image}`}
-				/>
-			</div>
+<Parallax sections={features.length + 5} config={{stiffness: 0.2, damping: 1}} sectionHeight={200}>
+	<ParallaxLayer rate={0} span={2}>
+		<div class="py-72 bg-gradient-to-b dark:from-zinc-900 dark:to-slate-900 text-center">
+			<h1 class="text-4xl text-zinc-100 py-2">
+				Welcome to the Aethor website
+			</h1>
+			<p class="text-xl text-zinc-100 py-2"><b>
+				The best suggestions bot
+			</b></p>
 		</div>
+	</ParallaxLayer>
+
+	{#each features as feature, index}
+		<ParallaxLayer rate={1.25} offset={2 + index} span={1} class="dark:bg-slate-900">
+			
+			<div
+				class={`md:flex w-full p-2 bg-zinc-800 my-10 h-100 ${
+					index % 2 ? 'md:ml-auto md:mr-0 text-right' : 'md:mr-auto md:ml-0 md:flex-row-reverse'
+				}`}
+			>
+				<div class="m-auto p-4">
+					<h3 class="md:text-5xl text-2xl dark:text-zinc-200 text-sky-700 py-3"><b>{feature.name}</b></h3>
+					<p class="md:text-2xl text-lg dark:text-zinc-400 text-blue-800">{feature.description}</p>
+				</div>
+				<div class="m-auto p-1">
+					<img
+						class="md:h-96 md:object-cover w-auto h-auto rounded-xl"
+						alt={feature.name}
+						src={`/features/${feature.image}`}
+					/>
+				</div>
+			</div>
+		</ParallaxLayer>
 	{/each}
-</div>
+</Parallax>
+
 <div class="flex justify-center">
 	<a
 		class={'hover:text-teal-100 bg-blue-800 text-white border-blue-900 border-4 p-4 text-2xl hover:bg-sky-800 hover:border-sky-900 duration-200 md:duration-700 text-center bg-blue hover:bg-blue-light font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded'}
