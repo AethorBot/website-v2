f379a58838940c5f2b5fbe3ff3a1babe8741ba7a
stef-the <stefthedoggo4@gmail.com>
2022-05-09 08:31:59 -0700

    :eyes:

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -47,34 +47,38 @@
 		}
 	];
 </script>
-
-<nav
-	class="w-full gap-2 bg-zinc-500 dark:bg-zinc-900 dark:text-zinc-200 p-4 text-sky-200 flex flex-col md:flex-row"
+<!--  Add "sticky top-0 z-50" to class below for sticky nav -->
+<nav 
+	class="w-full gap-2 bg-zinc-300 dark:bg-zinc-900 dark:text-zinc-200 p-4 text-slate-900 flex flex-col md:flex-row"
 >
 	{#each buttons as button}
 		<a
 			href={button.l}
-			class="dark:hover:bg-zinc-700 duration-150 py-1 px-2 text-lg rounded-lg"
+			class="hover:bg-zinc-900 hover:text-zinc-300 dark:hover:bg-zinc-200 dark:hover:text-zinc-900 duration-150 py-1 px-2 text-lg rounded-lg"
 			>{button.n}</a
 		>
 	{/each}
 
 	<button
-		class="md:last:ml-auto dark:hover:bg-zinc-700 duration-150 py-1 px-2 text-lg rounded-lg"
+		class="md:last:ml-auto hover:bg-zinc-900 hover:text-zinc-300 dark:hover:bg-zinc-200 dark:hover:text-zinc-900 duration-150 py-1 px-2 text-lg rounded-lg"
 		on:click={theme}>Change theme</button
 	>
 </nav>
 <slot id="top" />
-<div class="sticky w-full flex justify-end bottom-0 pb-3 pr-5 transition">
-	<div class="text-gray-400 hover:text-blue-400 transition p-2">
-		<a href="#top">Scroll to top</a>
-	</div>
+<div class="sticky w-full flex justify-end bottom-0 pb-3 pr-5 transition duration-150">
+	<a href="#top"><div class="text-gray-400 hover:text-slate-400 hover:bg-slate-800 transition duration-150 p-2 rounded-lg">
+		Top
+	</div></a>
 </div>
-<footer class="w-full bg-sky-500 dark:bg-sky-900 p-4">
+<footer class="w-full bg-gradient-to-t from-zinc-300 to-slate-300 dark:from-zinc-900 dark:to-slate-900 p-4">
 	<Paragraph>
 		<p>
-			<a href="https://top.gg/bot/870383692403593226">Top.gg</a>
-			<a href="https://discord.gg/zwUQGAG4cP">Support/Discord</a>
+			<a href="https://top.gg/bot/870383692403593226" class="hover:bg-zinc-900 hover:text-zinc-300 dark:hover:bg-zinc-200 dark:hover:text-zinc-900 duration-150 py-1 px-2 text-lg rounded-lg">
+				Top.gg
+			</a>
+			<a href="https://discord.gg/zwUQGAG4cP" class="hover:bg-zinc-900 hover:text-zinc-300 dark:hover:bg-zinc-200 dark:hover:text-zinc-900 duration-150 py-1 px-2 text-lg rounded-lg">
+				Support/Discord
+			</a>
 		</p>
 	</Paragraph>
 </footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,7 @@
 	import Addbutton from '../components/addbutton.svelte';
 
 	import { Parallax, ParallaxLayer } from 'svelte-parallax';
+	let parallax;
 
 	export const ssr = true;
 	export const prerender = true;
@@ -35,29 +36,31 @@
 	console.log(features.length+1)
 </script>
 
-<Parallax sections={features.length + 5} config={{stiffness: 0.2, damping: 1}} sectionHeight={200}>
-	<ParallaxLayer rate={0} span={2}>
-		<div class="py-72 bg-gradient-to-b dark:from-zinc-900 dark:to-slate-900 text-center">
-			<h1 class="text-4xl text-zinc-100 py-2">
-				Welcome to the Aethor website
-			</h1>
-			<p class="text-xl text-zinc-100 py-2"><b>
-				The best suggestions bot
-			</b></p>
+<Parallax sections={features.length + 7} config={{stiffness: 0.2, damping: 1}} sectionHeight={200}>
+	<ParallaxLayer rate={0.5} span={2}>
+		<div class="py-72 bg-gradient-to-b from-zinc-300 to-slate-300 dark:from-zinc-900 dark:to-slate-900">
+			<div class="text-center">
+				<h1 class="text-4xl text-zinc-900 dark:text-zinc-100 py-2">
+					Welcome to the Aethor website
+				</h1>
+				<p class="text-xl text-zinc-900 dark:text-zinc-100 py-2"><b>
+					The best suggestions bot
+				</b></p>
+			</div>
 		</div>
 	</ParallaxLayer>
 
 	{#each features as feature, index}
-		<ParallaxLayer rate={1.25} offset={2 + index} span={1} class="dark:bg-slate-900">
+		<ParallaxLayer rate={1.25} offset={2 + index} span={1}>
 			
 			<div
-				class={`md:flex w-full p-2 bg-zinc-800 my-10 h-100 ${
+				class={`md:flex w-full p-2 my-10 h-100 ${
 					index % 2 ? 'md:ml-auto md:mr-0 text-right' : 'md:mr-auto md:ml-0 md:flex-row-reverse'
 				}`}
 			>
 				<div class="m-auto p-4">
-					<h3 class="md:text-5xl text-2xl dark:text-zinc-200 text-sky-700 py-3"><b>{feature.name}</b></h3>
-					<p class="md:text-2xl text-lg dark:text-zinc-400 text-blue-800">{feature.description}</p>
+					<h3 class="md:text-5xl text-2xl dark:text-zinc-200 text-zinc-900 py-3"><b>{feature.name}</b></h3>
+					<p class="md:text-2xl text-lg dark:text-zinc-400 text-zinc-800">{feature.description}</p>
 				</div>
 				<div class="m-auto p-1">
 					<img
@@ -69,17 +72,21 @@
 			</div>
 		</ParallaxLayer>
 	{/each}
-</Parallax>
 
-<div class="flex justify-center">
-	<a
-		class={'hover:text-teal-100 bg-blue-800 text-white border-blue-900 border-4 p-4 text-2xl hover:bg-sky-800 hover:border-sky-900 duration-200 md:duration-700 text-center bg-blue hover:bg-blue-light font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded'}
-		href="https://discord.com/oauth2/authorize?client_id=870383692403593226&permissions=117824&scope=bot%20applications.commands"
-		target="_blank"
-	>
-		Add Aethor</a
-	>
-</div>
+	<ParallaxLayer offset={features.length+4}>
+		<div class="flex justify-center">
+			<a
+				class={'hover:text-teal-100 bg-blue-800 text-white border-blue-900 border-4 p-4 text-2xl hover:bg-sky-800 hover:border-sky-900 duration-200 md:duration-700 text-center bg-blue hover:bg-blue-light font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded'}
+				href="https://discord.com/oauth2/authorize?client_id=870383692403593226&permissions=117824&scope=bot%20applications.commands"
+				target="_blank"
+				id="download"
+			>
+				Add Aethor</a
+			>
+		</div>
+	</ParallaxLayer>
+
+</Parallax>
 
 <Paragraph>
 	<!-- <p>
