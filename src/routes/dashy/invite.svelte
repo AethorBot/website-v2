336ac7eaa17ce1db8113ffaa8130c7b6dac42eb9
Tricked <tricked@tricked.pro>
2022-08-20 11:59:44 +0200

    invite

---
+++ src/routes/dashy/invite.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	import { DISCORD, DASHY_INVITE } from '../../consts';
+	/** * @type {import('@sveltejs/kit').Load} */
+	export async function load() {
+		return {
+			status: 307,
+			redirect: DASHY_INVITE
+		};
+	}
+</script>
+
+<a href={DASHY_INVITE}>{DASHY_INVITE}</a>
