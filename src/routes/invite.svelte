7cd2ad5e37b5757ba9cd1a790e1627482605dd91
Tricked <tricked@tricked.pro>
2022-08-20 14:16:59 +0200

    add normal invite redirect

---
+++ src/routes/invite.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	import { INVITE } from '../consts';
+	/** * @type {import('@sveltejs/kit').Load} */
+	export async function load() {
+		return {
+			status: 307,
+			redirect: INVITE
+		};
+	}
+</script>
+
+<a href={INVITE}>{INVITE}</a>
